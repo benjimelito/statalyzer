d7a6b31dcf3bb1d5c6ad1642f79323ab6e031fe1
benjimelito <melito.ben@gmail.com>
2016-12-15 23:09:46 -0600

    Trying to get cheerio module working with webpack

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const app = express();
 const routes = require('./routes/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.use('/', routes);
 
