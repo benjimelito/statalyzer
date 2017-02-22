748cf09dd0ba3a1e97f868a04d88085778200bac
benjimelito <melito.ben@gmail.com>
2017-02-22 13:46:36 -0500

    Updated DB schema

--- server/app.js
+++ server/app.js
@@ -19,9 +19,9 @@ if(cluster.isMaster) {
         cluster.fork();
     }
 
-    cluster.on('online', function(worker) {
-        console.log('Worker ' + worker.process.pid + ' is online');
-    });
+    // cluster.on('online', function(worker) {
+    //     console.log('Worker ' + worker.process.pid + ' is online');
+    // });
 
     cluster.on('exit', function(worker, code, signal) {
         console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
