af8ec4c30aac3c2ad1ab133d17f1fe703f1f6451
Melito <bmelito@mitre.org>
2018-01-13 15:00:42 -0500

    More work on tests

--- server/routes/statsOnDate.js
+++ server/routes/statsOnDate.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const stats = require('../controllers/statsOnDateController');
-
-const knex = require('knex')({
-  client: 'postgresql',
-  connection: {
-    database: 'statlyzer_data'
-  }
-});
+const knex = require('knex')
+// ({
+//   client: 'postgresql',
+//   connection: {
+//     database: 'statlyzer_data'
+//   }
+// });
 
 router.post('/stats', stats.insertStatsOnDate)
 router.get('/stats', stats.getStatsOnDate)
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,12 +3,20 @@ process.env.NODE_ENV = 'test';
 var chai = require('chai');
 var should = chai.should();
 var chaiHttp = require('chai-http');
-var server = require('../app');
+var server = require('../server/app.js');
 
 
 
 chai.use(chaiHttp);
 
 describe('API Routes', function() {
-	
+	it('should create statsOnDate entries', function(done) {
+		chai.request(server)
+		.post('/stats')
+		.send({"team": "Boston"})
+		.end(function(err, res) {
+			res.should.have.status(200);
+			done();
+		})
+	})
 });
