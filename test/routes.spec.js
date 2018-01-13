70135966fb74a5bf946f2f7abfeb51cdedbf3ec4
Melito <bmelito@mitre.org>
2018-01-13 14:27:22 -0500

    Adding tests

---
+++ test/routes.spec.js
@@ -0,0 +1,14 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var should = chai.should();
+var chaiHttp = require('chai-http');
+var server = require('../app');
+
+
+
+chai.use(chaiHttp);
+
+describe('API Routes', function() {
+	
+});
